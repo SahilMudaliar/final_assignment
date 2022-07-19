1590e69d81fa846e64df294e18c8dc161f9d4017
sahil_mudaliar <sahilmudaliar7777@gmail.com>
2022-07-19 15:58:51 +0530

    updating commit

---
+++ CRUD/src/app/edit-form/edit-form.component.ts
@@ -0,0 +1,96 @@
+import { Component, Input, OnInit,Output,EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import {FormControl,FormGroup, Validators} from "@angular/forms"
+import {GetUsersService} from '../services/get-users.service'
+import { UserModel } from '../user/user-model';
+
+
+@Component({
+  selector: 'app-edit-form',
+  templateUrl: './edit-form.component.html',
+  styleUrls: ['./edit-form.component.css']
+})
+export class EditFormComponent implements OnInit {
+
+  constructor(private api:GetUsersService) {
+    
+  }
+   
+
+ // reactive form in angular
+
+  user_details = new FormGroup({
+    firstname: new FormControl("",[Validators.required]),
+    middle_name: new FormControl(""),
+    lastname: new FormControl("",[Validators.required]),
+    contact: new FormControl("",Validators.required),
+    role: new FormControl("",[Validators.required]),
+    address: new FormControl("",[Validators.required])
+
+
+  })
+
+  @Output() parent_function : EventEmitter<any> = new EventEmitter() // for using function written in parent component
+  @Output() load_data_in_app:EventEmitter<any> = new EventEmitter() // it will help to load data when ever a user submit form
+  @Input()
+  user_Details:any
+
+
+ 
+  
+  // for getting users detail passed from table commponent for edit a row
+ 
+  @Input()
+  update_check :string = "string" // for getting data from db
+  roles = Roles //enum for choosing role
+  ngOnInit(): void {
+    
+
+  }
+  
+ 
+   //getters for reactive form
+
+
+   get firstname(){
+    return this.user_details.get("firstname")
+  }
+  get lastname(){
+    return this.user_details.get("lastname")
+  }
+  get contact(){
+    return this.user_details.get("contact")
+  }
+  get role(){
+    return this.user_details.get("role")
+  }
+  get address(){
+    return this.user_details.get("address")
+    
+  }
+//###################################################################################################################################################
+  // when ever  a form submit button is clicked then this function gona execute
+  
+  table_data(data:any){
+    this.api.update_user(this.update_check,data).subscribe((result)=>{
+      console.log(result)
+      console.log(data)
+      console.log(this.update_check)
+      this.parent_function.emit(true)
+      this.load_data_in_app.emit(true)
+      
+    })
+  }
+//################################################################################################################################################
+
+
+
+
+
+
+
+}
+enum Roles{
+  SuperAdmin=1,
+  Admin=2,
+  user=0
+}
